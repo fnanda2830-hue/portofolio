f9bfbd8428f3d869d855646f7c10159844cb4776
LEFIENNA <fnanda2830@gmail.com>
2025-10-07 12:17:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const tombolSapa=document.getElementById('sapaButton');
 tombolSapa.addEventListener('clik',function(){
-  aletr('Hy! Xie-xie for coming!');
+  alert('Hy! Xie-xie for coming!');
 });
