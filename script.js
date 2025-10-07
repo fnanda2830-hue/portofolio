ca4747dd6cdfd7416832eea8d5b414fdd627cb7f
LEFIENNA <fnanda2830@gmail.com>
2025-10-07 12:04:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // 1. Pilih tombol berdasarkan ID-nya
 const tombolSapa = document.getElementById('sapaButton');
 // 2. Tambahan 'event listener' untuk aksi 'clik'
-tombolSapa.addEventListener('clik', function(){
+tombolSapa.addEventListener('clik', function() {
   // 3. Jalankan kode ini ketika tombol di-klik
   aletr('Hy! Xie-xie for coming!');
 });
